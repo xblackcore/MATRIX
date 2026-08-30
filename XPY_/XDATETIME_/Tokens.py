355decc267c0693d213a7f38d059a25688031e31
sirdoyle <blackcore.ada@gmail.com>
2026-08-30 06:19:05 +0200

    [Σ3 / Δ5 / ∅21] --- [Σ] Added. - desktop.ini - desktop_/IconResource.ico - desktop_/IconResource.png --- [Δ] Modified. - .gitignore - XCSS.css - XCSS_/XKEYFRAMES.css - XPY.py - XPY_/pyproject.toml --- [∅] Deleted. - XCSS_/XMEDIA.css - XPY_/SCT.py - XPY_/XCTYPES.py - XPY_/XDATETIME.py - XPY_/XDATETIME_/Tokens.py - XPY_/XEXCEPT.py - XPY_/XINPUT.py - XPY_/XINSPECT.py - XPY_/XLANGDETECT.py - XPY_/XOS.py - XPY_/XPATHLIB.py - XPY_/XPIP.py - XPY_/XPRINT.py - XPY_/XREQUESTS.py - XPY_/XSHUTIL.py - XPY_/XSUBPROCESS.py - XPY_/XSYS.py - XPY_/XTKINTER.py - XPY_/XTRACEBACK.py - XPY_/XTTS.py - XPY_/XVOSK.py ---

--- XPY_/XDATETIME_/Tokens.py
+++
@@ -1,9 +0,0 @@
-Tokens = {
-    "Year"        : lambda dt: f"{dt.year}",
-    "Month"       : lambda dt: f"{dt.month:02d}",
-    "Day"         : lambda dt: f"{dt.day:02d}",
-    "Hour"        : lambda dt: f"{dt.hour:02d}",
-    "Minute"      : lambda dt: f"{dt.minute:02d}",
-    "Second"      : lambda dt: f"{dt.second:02d}",
-    "Microsecond" : lambda dt: f"{dt.microsecond:02d}",
-}
