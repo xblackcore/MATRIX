d86e5f8c462272ac908954d004b41296ea44a9f2
sirdoyle <blackcore.doyle@gmail.com>
2026-08-31 16:57:05 +0200

    [Σ2 / Δ3 / ∅1] --- [Σ] Added. - CODEX.md - XCSS_/XKEYFRAMES_/Transform.css --- [Δ] Modified. - XPY.py - XPY_/XPY.py - index.html --- [∅] Deleted. - XPY_/XPRINT.py ---

--- XPY_/XPY.py
+++ XPY_/XPY.py
@@ -2,36 +2,26 @@
 #┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 #region    [M]  										   ┃
 # __doc__
-"""
-    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-    # [XPY.py]
-    ## META-Python main entry point
-    \n┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
-    ## [+]
-    - |
-    \n┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
-    ## [!]
-    - |
-    \n┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
-    ## [?]
-    - |
-    \n┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
-    ## [>]
-    - |
-    \n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+"""---
+    # [XPY] META-Python
+    Main entry point.
+
+    ---
+    ## [class]
+    ```python
+        class XPY_: # XPY template class object
+    ```
+
+    ---
+    ## [def]
+    ```python 
 """
 # __author__
 __author__ = "Sir. Doyle; S.Class Astartes at BlackCoreΔ"
 # __version__
 __version__ = "1.0"
+__version_info__ = (1, 0, 0)
 # __annotations__
-DEBUG: bool = False
-# import
-try: from XPRINT import Error, Print, Box
-except ImportError as E:
-    print(f"\033[91m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\033[0m")
-    print(f"\033[91m[{__file__[0].upper() + __file__[1:]}] ImportError:{E}\033[0m")
-    print(f"\033[91m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\033[0m")
 #endregion [M]  										   ┃
 #┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 #┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
@@ -60,48 +50,15 @@ def F():
 #┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 #┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 #region    [X] 											   ┃
+class X_:
+    ...
+def X():
+    ...
 #endregion [X] 											   ┃
 #┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 #┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 #┃ [.U] 												   ┃
 """
-class XPY_:
-    def __init__(self,
-        Debug         = False,
-        BoxStyle      = "Cyan",
-        BoxTextStyle  = "White",
-        XPYTextStyle  = "White",
-        BoxTitle      = __file__
-    ):
-        self.Debug         = Debug
-
-        self.BoxStyle      = BoxStyle
-        self.BoxTextStyle  = BoxTextStyle
-        self.BoxTitle      = BoxTitle
-
-        self.XPYTextStyle  = XPYTextStyle
-        self._CallsStack   = 0
-    def Say(self, I):
-        print(f"\033[96m[XPY]\033[0m {I}")
-    def X(self,
-        I,
-        Sub = None,
-        End = None
-    ):
-        if self._CallsStack == 0: # I == Main
-            Box(
-                Title = self.BoxTitle,
-                Style = self.BoxStyle)
-
-        elif self._CallsStack >= 1:
-            Box(Style = self.BoxStyle, Section=1)
-
-        if callable(I):
-            self.Say(f"callable({I})")
-            self._CallsStack +=1
-            self.Say(f"_CallsStack = {self._CallsStack}")
-            self.Say(f"try: {I}()")
-            I()
 
 """
 #┃ [.U] 												   ┃
